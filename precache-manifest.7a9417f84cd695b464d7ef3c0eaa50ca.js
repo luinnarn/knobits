d96f09a69659494aa8824f8444440ee879e9de80
ndragic <ndragic@execom.eu>
2020-04-05 14:33:55 +0200

    Updates

---
+++ precache-manifest.7a9417f84cd695b464d7ef3c0eaa50ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c65cdcc9aade8fd6668c7ac6c1717e0",
+    "url": "/knobits/index.html"
+  },
+  {
+    "revision": "93e0d03bb0f86c034474",
+    "url": "/knobits/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aef341745d0ec45c7b38",
+    "url": "/knobits/static/js/2.c1b3951e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/knobits/static/js/2.c1b3951e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93e0d03bb0f86c034474",
+    "url": "/knobits/static/js/main.46481a5a.chunk.js"
+  },
+  {
+    "revision": "24b85c2dcd2ca0211929",
+    "url": "/knobits/static/js/runtime-main.74cabeb4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/knobits/static/media/logo.5d5d9eef.svg"
+  }
+]);
